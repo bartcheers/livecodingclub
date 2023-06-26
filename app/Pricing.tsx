2ed78ff1609123bcb859d3348d4fdade95168159
Bart <bart@pagespeed.guru>
2023-06-26 08:49:32 -0700

    fix lint

--- app/Pricing.tsx
+++ app/Pricing.tsx
@@ -61,7 +61,7 @@ export function Pricing() {
           </p>
         </div>
         <p className="mx-auto mt-6 max-w-2xl text-center text-lg leading-8 text-gray-300">
-          To celebrate our launch, we're offering a life-time plan for early adopters.
+          To celebrate our launch, we&apos;re offering a life-time plan for early adopters.
         </p>
         <div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {tiers.map((tier) => (
