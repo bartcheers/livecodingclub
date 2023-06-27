177abdf32fcdf01814eae209470874813df9bb6d
Bart <bart@pagespeed.guru>
2023-06-26 21:54:33 -0700

    Fix text login

--- app/login.tsx
+++ app/login.tsx
@@ -7,7 +7,7 @@ export const Login = () => {
   return (
 
 
-    <button className="flex items-center gap-2 text-black rounded-lg bg-white w-fit mt-8 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-neutral-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400" onClick={() => signIn('google')}>
+    <button className="flex items-center gap-2 text-black rounded-lg bg-white w-fit mt-8 px-3 py-2 text-sm font-semibold shadow-sm hover:bg-neutral-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400" onClick={() => signIn('google')}>
       <GoogleIcon />
       Sign in with Google
     </button>
