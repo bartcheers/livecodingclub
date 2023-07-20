8367b0b49df61ea52275db7cc1e2c3736cf95b58
Bart <bart@pagespeed.guru>
2023-07-20 08:15:43 -0700

    Change main cta label to be more descriptive

--- app/CreatePostButton.tsx
+++ app/CreatePostButton.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { experimental_useFormStatus } from 'react-dom';
-import { Edit3, Loader } from 'react-feather';
+import { Loader, Video } from 'react-feather';
 
 export const CreatePostButton = () => {
   const { pending } = experimental_useFormStatus();
@@ -13,8 +13,8 @@ export const CreatePostButton = () => {
     <Link
       href='/post'
       className='group flex justify-center items-center flex-1 border border-neutral-700 rounded-lg ml-2 hover:bg-neutral-800 hover:border-neutral-800 transition-colors'>
-      <span>What are you working on?</span>
-      <Edit3 className='w-0 group-hover:flex group-hover:w-5 group-hover:ml-2 h-5 transition-all' />
+      <span>Start a coding session</span>
+      <Video className='w-0 group-hover:flex group-hover:w-5 group-hover:ml-2 h-5 transition-all' />
     </Link>
   );
 };
