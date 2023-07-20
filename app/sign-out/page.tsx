b2fbbbe7275be1dbc534203dfd998f8a469f0b0f
Bart <bart@pagespeed.guru>
2023-07-19 21:26:28 -0700

    redirect to home on log out

--- app/sign-out/page.tsx
+++ app/sign-out/page.tsx
@@ -3,8 +3,12 @@
 import { useSession } from 'next-auth/react';
 import Logout from './Logout';
 import Login from '../shared/Login';
+import { redirect } from 'next/navigation';
 
 export default function SignOut() {
   const { data: session } = useSession();
+
+  if (!session) redirect('/');
+
   return session ? <Logout /> : <Login />;
 }
